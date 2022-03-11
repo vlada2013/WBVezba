8b4ff017885f74c981e67358e8c1383b5f040f2a
Aleksandar Stojimirovic <aleksandar.stojimirovic@bridgewaterlabs.com>
2022-03-11 09:26:30 +0100

    Add FileReaderHelper

--- src/test/java/BooksApi/GetBooks.java
+++ src/test/java/BooksApi/GetBooks.java
@@ -4,12 +4,9 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-
-
 
 public class GetBooks {
 
@@ -17,15 +14,13 @@ public class GetBooks {
 
     @Test
     public void getAllBooks() {
-
-
         Response responseBody=  given()
                 .when()
                 .get("https://simple-books-api.glitch.me/books")
                 .then().extract().response();
 
         JsonPath jsonPath=responseBody.jsonPath();
-        assertThat("Name value.", jsonPath.get("name[0]"), equalTo("company"));
+        assertThat("Name value.", jsonPath.get("name[0]"), equalTo("The Russian"));
         String name = jsonPath.get("name[0]");
         System.out.println("Name received from Response " + name);
     }
